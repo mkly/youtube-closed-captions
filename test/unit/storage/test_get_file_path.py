eb73fb172db70e67c5811989ac6dc216fac1e1b4
Mike Lay <mike@mkly.io>
2016-11-06 19:16:16 -0500

    Allow for valid youtube ids with dash
    * Change regex to use `\w` to make it more obvious that `_` is already
      included.
    * Add `-` to handle additional valid ids
    * Add test coverage for underscore and dash
    
    Fixes #1

--- test/unit/storage/test_get_file_path.py
+++ test/unit/storage/test_get_file_path.py
@@ -12,6 +12,18 @@ class TestGetFilePath(unittest.TestCase):
         expected = 'subtitle_{0}.en.vtt'.format(video_id)
         self.assertEqual(expected, storage.get_file_path())
 
+    def test_valid_with_underscore(self):
+        video_id = 'w8U6VI_51Bg'
+        storage = Storage(video_id)
+        expected = 'subtitle_{0}.en.vtt'.format(video_id)
+        self.assertEqual(expected, storage.get_file_path())
+
+    def test_valid_with_dash(self):
+        video_id = 'x1E0AzpWM-o'
+        storage = Storage(video_id)
+        expected = 'subtitle_{0}.en.vtt'.format(video_id)
+        self.assertEqual(expected, storage.get_file_path())
+
     def test_invalid(self):
         video_id = 'vsdoi&*@//../../'
         storage = Storage(video_id)
--- ytcc/storage.py
+++ ytcc/storage.py
@@ -10,12 +10,12 @@ class Storage():
         self.video_id = video_id
 
     def get_file_path(self) -> str:
-        if re.search(r'[^A-z0-9]', self.video_id):
+        if re.search(r'[^\w-]', self.video_id):
             raise ValueError(
                 'Invalid video id attempting to write to filesystem')
 
         return 'subtitle_{0}.en.vtt'.format(
-            re.sub(r'[^A-z0-9]', '', self.video_id))
+            re.sub(r'[^\w-]', '', self.video_id))
 
     def remove_file(self) -> None:
         os.remove(self.get_file_path())
